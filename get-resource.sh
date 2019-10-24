60364ac26217105e571666dc9304a6118d94e90d
Derek Higgins <derekh@redhat.com>
2019-10-24 06:29:59 -0400

    Turn off globbing on curl
    Instruct curl to stop treating square brackets as special, to
    allow for IPv6.

--- get-resource.sh
+++ get-resource.sh
@@ -17,25 +17,25 @@ TMPDIR=$(mktemp -d -p /shared/tmp)
 # get header info from the cache
 ls -l
 if [ -n "$CACHEURL" -a ! -e $FFILENAME.headers ] ; then
-    curl --verbose --fail -O "$CACHEURL/$FFILENAME.headers" || true
+    curl -g --verbose --fail -O "$CACHEURL/$FFILENAME.headers" || true
 fi
 
 # Download the most recent version of IPA
 if [ -e $FFILENAME.headers ] ; then
     ETAG=$(awk '/ETag:/ {print $2}' $FFILENAME.headers | tr -d "\r")
     cd $TMPDIR
-    curl --verbose --dump-header $FFILENAME.headers -O https://images.rdoproject.org/train/rdo_trunk/$SNAP/$FFILENAME --header "If-None-Match: $ETAG"
+    curl -g --verbose --dump-header $FFILENAME.headers -O https://images.rdoproject.org/train/rdo_trunk/$SNAP/$FFILENAME --header "If-None-Match: $ETAG"
     # curl didn't download anything because we have the ETag already
     # but we don't have it in the images directory
     # Its in the cache, go get it
     ETAG=$(awk '/ETag:/ {print $2}' $FFILENAME.headers | tr -d "\"\r")
     if [ ! -s $FFILENAME -a ! -e /shared/html/images/$FILENAME-$ETAG/$FFILENAME ] ; then
         mv /shared/html/images/$FFILENAME.headers .
-        curl --verbose -O "$CACHEURL/$FILENAME-$ETAG/$FFILENAME"
+        curl -g --verbose -O "$CACHEURL/$FILENAME-$ETAG/$FFILENAME"
     fi
 else
     cd $TMPDIR
-    curl --verbose --dump-header $FFILENAME.headers -O https://images.rdoproject.org/train/rdo_trunk/$SNAP/$FFILENAME
+    curl -g --verbose --dump-header $FFILENAME.headers -O https://images.rdoproject.org/train/rdo_trunk/$SNAP/$FFILENAME
 fi
 
 if [ -s $FFILENAME ] ; then
