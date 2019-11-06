163543af931ebec8e6f9c1096925db21bbc4d0ed
Stephen Benjamin <stephen@redhat.com>
2019-11-06 09:25:41 -0500

    Allow overriding the URL

--- get-resource.sh
+++ get-resource.sh
@@ -3,6 +3,7 @@
 
 # Which image should we use
 SNAP=${1:-current-tripleo}
+IPA_BASEURI=${IPA_BASEURI:-https://images.rdoproject.org/train/rdo_trunk/$SNAP/}
 
 FILENAME=ironic-python-agent
 FILENAME_EXT=.tar
@@ -24,7 +25,7 @@ fi
 if [ -e $FFILENAME.headers ] ; then
     ETAG=$(awk '/ETag:/ {print $2}' $FFILENAME.headers | tr -d "\r")
     cd $TMPDIR
-    curl --verbose --dump-header $FFILENAME.headers -O https://images.rdoproject.org/train/rdo_trunk/$SNAP/$FFILENAME --header "If-None-Match: $ETAG"
+    curl --verbose --dump-header $FFILENAME.headers -O $IPA_BASEURI/$FFILENAME --header "If-None-Match: $ETAG"
     # curl didn't download anything because we have the ETag already
     # but we don't have it in the images directory
     # Its in the cache, go get it
@@ -35,7 +36,7 @@ if [ -e $FFILENAME.headers ] ; then
     fi
 else
     cd $TMPDIR
-    curl --verbose --dump-header $FFILENAME.headers -O https://images.rdoproject.org/train/rdo_trunk/$SNAP/$FFILENAME
+    curl --verbose --dump-header $FFILENAME.headers -O $IPA_BASEURI/$FFILENAME
 fi
 
 if [ -s $FFILENAME ] ; then
